3cd2b3b71e0b33d41196d7bdc39c49473b5e071f
Fabián Lasso <fabolasso90@gmail.com>
2024-02-27 21:24:32 -0500

    sign in page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/Login.jsx";
 
 //create your first component
 const Layout = () => {
@@ -23,14 +24,12 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<Login />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </div>
---
+++ src/front/js/pages/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useState, useEffect } from "react";
+import Swal from "sweetalert";
+import background from "../../img/background.png";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+import "../../styles/login.css";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+  const [inputUser, setInputUser] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  return (
+    <div
+      className="container"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div>
+        <div className="form-signin">
+          <form className="text-center">
+            <div className="form-floating m-1">
+              <input
+                type="text"
+                className="form-control"
+                id="user"
+                placeholder="User name"
+                onChange={(e) => setInputUser(e.target.value)}
+                value={inputUser}
+              />
+              <label htmlFor="user">User name</label>
+            </div>
+            <div className="form-floating m-1">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                onChange={(e) => setInputPassword(e.target.value)}
+                value={inputPassword}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <div>
+              <button
+                className="w-100 btn btn-sm btn-primary mb-1"
+                type="button"
+                onClick={() => {
+                  if (inputUser !== "" && inputPassword !== "") {
+                    save();
+                  } else {
+                    Swal.fire({
+                      icon: "error",
+                      title: "Error",
+                      text: "Please fill out all input fields",
+                    });
+                  }
+                }}
+              >
+                Log in
+              </button>
+              <button
+                className="w-100 btn btn-sm btn-secondary mb-2"
+                type="button"
+                onClick={() => navigate("/signup")}
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
